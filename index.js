22c8ada0953b8469727797eb9cc911a9db270c71
KajalThakur7 <kajalthakur312001@gmail.com>
2023-05-15 11:26:47 -0700

    tested for git

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-const express = require('express');
-const connectDB = require('./server/database/connection')
-const userRoute = require('./server/router/route')
-const bodyParser = require('body-parser')
+const express = require("express");
+const connectDB = require("./server/database/connection");
+const userRoute = require("./server/router/route");
+const bodyParser = require("body-parser");
 // const cors = require("cors");
 
-const app = express()
+const app = express();
 PORT = 6000;
 
+console.log("testing out the git");
 
-
-connectDB()
+connectDB();
 
 //middlewares
 //bodyParser handle http request
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cors());
-app.use('/', userRoute)
+app.use("/", userRoute);
 
-
-
-
-app.listen(PORT,(req,res)=>{
-    console.log(`server is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, (req, res) => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
