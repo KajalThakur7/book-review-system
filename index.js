289a9751b796b7fa66993801a171fea9a2792064
KajalThakur7 <kajalthakur312001@gmail.com>
2023-05-18 18:21:23 -0700

    swagger File

--- index.js
+++ index.js
@@ -3,31 +3,24 @@ const connectDB = require("./server/database/connection");
 const userRoute = require("./server/router/route");
 const bodyParser = require("body-parser");
 
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml')
-// const swaggerUi = require("swagger-ui-express");
-// const YAML = require('yamljs');
-// const swaggerDocument = YAML.load('./swagger.yaml')
-const cors = require("cors");
 
 const app = express();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-const PORT = 6000;
+PORT = 5000;
 
+///crud in mdb
 connectDB();
 
-// Middlewares
+//middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors()); // Enable CORS middleware
+app.use(cors());
 app.use("/", userRoute);
 
-
-
-// Serve Swagger API documentation
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, (req, res) => {
+  console.log(`server is running on http://localhost:${PORT}`);
 });
--- server/middlewares/requireAdmin.js
+++ server/middlewares/requireAdmin.js
@@ -1,19 +1,40 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-  console.log("token: ", token);
-
-  const decoded = jwt.verify(token, "this is dummy text");
-  console.log(decoded);
-
-  if (decoded.role != "admin") {
-    res.status(500).json({
-      status: 500,
-      message: "You are not an admin",
+  // Check if the Authorization header exists
+  if (!req.headers.authorization) {
+    res.status(401).json({
+      status: 401,
+      message: "Missing authorization header",
     });
     return;
   }
 
-  next();
+  // Extract the token from the Authorization header
+  const token = req.headers.authorization.split(" ")[1];
+  console.log("token: ", token);
+
+  try {
+    // Verify the token
+    const decoded = jwt.verify(token, "this is dummy text");
+    console.log(decoded);
+
+    // Check if the decoded token has the role of an admin
+    if (decoded.role !== "admin") {
+      res.status(403).json({
+        status: 403,
+        message: "You are not an admin",
+      });
+      return;
+    }
+
+    // Token is valid and user is an admin, continue to the next middleware
+    next();
+  } catch (error) {
+    // Handle JWT verification errors
+    res.status(401).json({
+      status: 401,
+      message: "Invalid or expired token",
+    });
+  }
 };
